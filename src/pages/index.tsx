ba53ba24139fc7a59d75fc65aafbd32e960bd07f
wesley <rafaelsanto10s@gmail.com>
2021-02-24 17:51:20 -0300

    estilo component countdown finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 // components 
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import Head from 'next/head';
 
 // estilo
 import styles from '../styles/pages/Home.module.css';
@@ -9,11 +12,16 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
