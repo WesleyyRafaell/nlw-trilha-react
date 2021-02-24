af6c662d607a0192cb145ced740deed39c40a545
wesley <rafaelsanto10s@gmail.com>
2021-02-24 12:05:44 -0300

    criando components home

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return(
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/23534161?s=460&u=c0134b805611621e2fcce7576b4a90ebd520f528&v=4" alt="perfil"/>
+      <div>
+        <strong>Wesley Rafael</strong>
+        <p>
+          <img src="icons/level.svg" alt="level"/>
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
